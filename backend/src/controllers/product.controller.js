3130e8016f11c00775d257c9d25b44d50ff8c9df
vijay Kumar <surya991837@gmail.com>
2025-07-24 01:35:14 +0530

    add email validator in the model

---
+++ backend/src/controllers/product.controller.js
@@ -0,0 +1,6 @@
+import User from "../models/user.model.js";
+import ApiResponse from "../utils/apiResponse.js";
+import ApiError from "../utils/apiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import Product from "../models/product.model.js";
+
