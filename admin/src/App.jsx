fc0805ba6cd378913512776fc2f75d3397abbe36
surya <vijayabout2001@gmail.com>
2025-07-25 11:49:42 +0530

    admin setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div 
-    className='bg-red-500' > App</div>
+    className='font-edu bg-mint-500 text-4xl text-center' > App</div>
   )
 }
 
-export default App
+export default  App;
